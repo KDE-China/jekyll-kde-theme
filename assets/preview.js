0f205c29f7a1f5eb531c4f51d8667c137da7e3bc
Carl Schwan <carl@carlschwan.eu>
2019-08-15 16:27:49 +0200

    Move js to aether sass

--- assets/preview.js
+++
@@ -1,5 +0,0 @@
-$(".preview").click(function() {
-    $("#kImagePreview img").attr('src', $(this).attr('src'));
-    $("#kImagePreview img").attr('alt', $(this).attr('alt'));
-    $("#kImagePreview").modal('show');
-});
